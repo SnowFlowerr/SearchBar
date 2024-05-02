d013edd03f18b9da6398a9c59f90cc632bd3b38c
Bhudeo Krit <Bhudeokrit@gmail.com>
2024-05-02 13:42:26 +0530

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import styles from './Search.module.css'
+import data from './data'
+
+export default function Search() {
+    const [inp,setInp]=useState("")
+    const [arr,setArr]=useState([])
+    function handleChange(e){
+        e.preventDefault()
+        setInp(e.target.value)
+    }
+    function handleSelect(select){
+        setInp(select)
+    }
+    useEffect(()=>{
+        setArr(data.filter((ele)=>ele.toLowerCase().includes(inp.toLowerCase().trim())))
+    },[inp])
+    
+
+    return (
+        <div className={styles.bigbox}>
+            <input type="text" onChange={handleChange} className={styles.inp} value={inp} placeholder='Search Here'/>
+            <div className={styles.options} id='option'>
+            {arr.map((ele) => <div className={styles.opt} onClick={()=>handleSelect(ele)}>{ele}</div>)}
+            </div>
+        </div>
+    )
+}
